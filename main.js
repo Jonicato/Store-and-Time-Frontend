079005499f5e758fa937ac0e2dcec0c113d9f1ee
Jonathan Rodriguez <jhonarodriguez98@gmail.com>
2022-08-24 23:14:38 -0500

    Interacción entre menús añadida

--- main.js
+++ main.js
@@ -2,15 +2,44 @@ const menuEmail = document.querySelector('.navbar-email');
 const burguerMenu = document.querySelector('.menu');
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const cartButton = document.querySelector('.navbar-shopping-cart')
+const asideCart = document.querySelector('.product-detail')
 
 menuEmail.addEventListener('click', toggleMenuDesktop);
-
 burguerMenu.addEventListener('click', toggleMenuMobile);
+cartButton.addEventListener('click', toggleAsideCart);
 
 function toggleMenuDesktop() {
+    const isAsideCartClosed = asideCart.classList.contains('inactive-cart');
+
+    if (!isAsideCartClosed) {
+        asideCart.classList.add('inactive-cart');
+    }
+
     desktopMenu.classList.toggle('inactive')
 }
 
 function toggleMenuMobile() {
+    const isAsideCartClosed = asideCart.classList.contains('inactive-cart');
+
+    if (!isAsideCartClosed) {
+        asideCart.classList.add('inactive-cart');
+    }
+
     mobileMenu.classList.toggle('inactive-mobile')
+}
+
+function toggleAsideCart() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive-mobile');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+    
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive-mobile');
+    }
+
+    if (!isDesktopMenuClosed) {
+        desktopMenu.classList.add('inactive');
+    }
+
+    asideCart.classList.toggle('inactive-cart');
 }
