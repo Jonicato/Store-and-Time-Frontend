ea89de7b53643180fef6703c3533dc9f0498bcf4
Jonicato <jhonarodriguez98@gmail.com>
2022-08-25 10:07:36 -0500

    Se agregaron más productos a la lista

--- main.js
+++ main.js
@@ -95,6 +95,56 @@ const arrProducts = [
         name: 'T-shirt',
         price: 139,
         image: 'https://images.pexels.com/photos/1311590/pexels-photo-1311590.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    },
+    {
+        name: 'Bike',
+        price: 120,
+        image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+    },
+    {
+        name: 'Laptop',
+        price: 320,
+        image: 'https://images.pexels.com/photos/18105/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    },
+    {
+        name: 'Spit',
+        price: 150,
+        image: 'https://images.pexels.com/photos/9385887/pexels-photo-9385887.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    },
+    {
+        name: 'Desktop',
+        price: 80,
+        image: 'https://images.pexels.com/photos/2811648/pexels-photo-2811648.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    },
+    {
+        name: 'Bed',
+        price: 200,
+        image: 'https://images.pexels.com/photos/164595/pexels-photo-164595.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    },
+    {
+        name: 'Skateboard',
+        price: 70,
+        image: 'https://images.pexels.com/photos/165236/pexels-photo-165236.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    },
+    {
+        name: 'Electric Guitar',
+        price: 230,
+        image: 'https://images.pexels.com/photos/2156327/pexels-photo-2156327.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    },
+    {
+        name: 'Backpack',
+        price: 50,
+        image: 'https://images.pexels.com/photos/3731256/pexels-photo-3731256.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    },
+    {
+        name: 'Cool cap',
+        price: 99,
+        image: 'https://images.pexels.com/photos/1878821/pexels-photo-1878821.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    },
+    {
+        name: 'T-shirt',
+        price: 139,
+        image: 'https://images.pexels.com/photos/1311590/pexels-photo-1311590.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
     }
 ];
 
